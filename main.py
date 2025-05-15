386b11337339ad8b617fd7e88fa296104e013113
EDGES2 <kirilltokarev826@gmail.com>
2025-05-15 11:29:50 +0200

    if-else changed to match

--- main.py
+++ main.py
@@ -33,27 +33,58 @@ def action_menu(graph):
     print("sortK - Perform topological sort (Kahn's algorithm)")
     print("exit - Exit program\n")
     while True:
+        # action = input("actions> ").lower()
+        # if action == "print":
+        #     graph.print_graph()
+        # elif action == "find":
+        #     from_node = int(input("from> "))
+        #     to_node = int(input("to> "))
+        #     exists = graph.find_edge(from_node, to_node)
+        #     if exists:
+        #         print(f"True: edge ({from_node},{to_node}) exists in the Graph")
+        #     else:
+        #         print(f"False: edge ({from_node},{to_node}) does not exist in the Graph")
+        # elif action == "sortk":
+        #     result = graph.topological_sort_kahn()
+        #     if result is None:
+        #         print("Graph contains at least one cycle - cannot perform topological sort")
+        #     else:
+        #         print("Topological order:", " ".join(map(str, result)))
+        # elif action == "exit":
+        #     break
+        # else:
+        #     print("Invalid action. Use 'print', 'find', 'sortK' or 'exit'")
         action = input("actions> ").lower()
-        if action == "print":
-            graph.print_graph()
-        elif action == "find":
-            from_node = int(input("from> "))
-            to_node = int(input("to> "))
-            exists = graph.find_edge(from_node, to_node)
-            if exists:
-                print(f"True: edge ({from_node},{to_node}) exists in the Graph")
-            else:
-                print(f"False: edge ({from_node},{to_node}) does not exist in the Graph")
-        elif action == "sortk":
-            result = graph.topological_sort_kahn()
-            if result is None:
-                print("Graph contains at least one cycle - cannot perform topological sort")
-            else:
-                print("Topological order:", " ".join(map(str, result)))
-        elif action == "exit":
-            break
-        else:
-            print("Invalid action. Use 'print', 'find', 'sortK' or 'exit'")
+        match action:
+            case 'help':
+                print("\nAvailable actions:")
+                print("help - For help")
+                print("print - Print graph in selected representation")
+                print("find - Check if edge exists")
+                print("sortK - Perform topological sort (Kahn's algorithm)")
+                print("exit - Exit program\n")
+            case 'print':
+                 graph.print_graph()
+            case 'find':
+                from_node = int(input("from> "))
+                to_node = int(input("to> "))
+                exists = graph.find_edge(from_node, to_node)
+                if exists:
+                    print(f"True: edge ({from_node},{to_node}) exists in the Graph")
+                else:
+                    print(f"False: edge ({from_node},{to_node}) does not exist in the Graph")
+            case 'sortk':
+                result = graph.topological_sort_kahn()
+                if result is None:
+                    print("Graph contains at least one cycle - cannot perform topological sort")
+                else:
+                    print("Topological order:", " ".join(map(str, result)))
+            case 'exit':
+                break
+            case _ :
+                print("Invalid action. Use 'print', 'find', 'sortK' or 'exit'")
+                
+                
 
 if __name__ == "__main__":
     main()
